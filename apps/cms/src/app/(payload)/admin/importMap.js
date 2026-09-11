46c779d907026017fdf27e2e082c13a0686cf0a9
cap1k0 <84158180+cap1k0@users.noreply.github.com>
2026-09-12 00:09:19 +0330

    Update importMap.js

--- apps/cms/src/app/(payload)/admin/importMap.js
+++ apps/cms/src/app/(payload)/admin/importMap.js
@@ -1,5 +1,7 @@
 import { VercelBlobClientUploadHandler } from '@payloadcms/storage-vercel-blob/client'
+import { RscEntryLexicalField } from '@payloadcms/richtext-lexical/rsc'
 
 export const importMap = {
   '@payloadcms/storage-vercel-blob/client#VercelBlobClientUploadHandler': VercelBlobClientUploadHandler,
+  '@payloadcms/richtext-lexical/rsc#RscEntryLexicalField': RscEntryLexicalField,
 }
