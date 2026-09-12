1d8e05917cf314f52bbb07e57934e12e5fee16fe
cap1k0 <84158180+cap1k0@users.noreply.github.com>
2026-09-12 20:01:13 +0330

    Update page.tsx

--- apps/blog/app/page.tsx
+++ apps/blog/app/page.tsx
@@ -43,7 +43,7 @@ export default async function BlogIndex() {
           <a href="https://bruca.space">
             <Logo />
           </a>
-          
+          <a
             href="https://bruca.space"
             className="text-sm text-neutral-600 hover:text-neutral-900"
           >
