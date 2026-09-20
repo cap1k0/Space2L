269593635e0d1db633fd7382b00d2f4b85439c37
cap1k0 <84158180+cap1k0@users.noreply.github.com>
2026-09-20 16:47:17 +0330

    Update page.tsx

--- apps/blog/app/page.tsx
+++ apps/blog/app/page.tsx
@@ -4,7 +4,7 @@ import { getPublishedArticles } from "./lib/cms";
 export const revalidate = 300;
 
 
-
+ 
 async function debugFetch() {
   const cmsUrl = process.env.NEXT_PUBLIC_CMS_URL || "https://cms.bruca.space";
   const params = new URLSearchParams();
